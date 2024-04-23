79b82c9c2e81d74976ace9adbadf7a783175e625
Punam-Dev <punam.pal.dev@gmail.com>
2024-04-23 20:41:27 +0530

    Added Tests

---
+++ tests/OpenWeatherForecast.Api.UnitTests/Abstractions/IntegrationTestWebApplicationFactory.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace OpenWeatherForecast.Api.UnitTests.Abstractions;
+
+public class IntegrationTestWebApplicationFactory : WebApplicationFactory<Program>
+{
+}
---
+++ tests/OpenWeatherForecast.Api.UnitTests/WeatherForecastTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using OpenWeatherForecast.Api.UnitTests.Abstractions;
+using System.Net;
+
+namespace OpenWeatherForecast.Api.UnitTests;
+
+public class WeatherForecastTests : BaseIntegrationTest
+{
+    public WeatherForecastTests(IntegrationTestWebApplicationFactory factory) : base(factory)
+    {
+    }
+
+    [Fact]
+    public async Task WeatherForecast_ShouldReturnSuccess()
+    {
+        // Act
+        var response = await HttpClient.GetAsync("weatherforecast");
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+}
