4b0530068a7ee1a0b8826e190794bf27bfbb52e4
Punam-Dev <punam.pal.dev@gmail.com>
2024-05-01 23:16:01 +0530

    fix s3 error

--- deploy/infra/modules/s3/main.tf
+++ deploy/infra/modules/s3/main.tf
@@ -5,48 +5,48 @@ resource "aws_s3_bucket" "elb_logs" {
   force_destroy = true
 }
 
-resource "aws_s3_bucket_policy" "policy" {
-  bucket = aws_s3_bucket.elb_logs.id
-  policy = <<POLICY
-{
-  "Id": "Policy",
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": ["${data.aws_elb_service_account.main.arn}"]
-      },
-      "Action": "s3:PutObject",
-      "Resource": "arn:aws:s3:::${var.app_name}-access-logs-${var.environment}/*"
-    },
-    {
-      "Action": ["s3:PutObject"],
-      "Effect": "Allow",
-      "Resource": "arn:aws:s3:::${var.app_name}-access-logs-${var.environment}/*",
-	    "Condition": {
-        "StringEquals": {
-          "s3:x-amz-acl": "bucket-owner-full-control"
-        }
-      },
-      "Principal": {
-	      "Service": "delivery.logs.amazonaws.com"
-      }
-    },
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "delivery.logs.amazonaws.com"
-      },
-      "Action": "s3:GetBucketAcl",
-      "Resource": "arn:aws:s3:::${var.app_name}-access-logs-${var.environment}"
-    }
-  ]
-}
-POLICY
-}
+# resource "aws_s3_bucket_policy" "policy" {
+#   bucket = aws_s3_bucket.elb_logs.id
+#   policy = <<POLICY
+# {
+#   "Id": "Policy",
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Effect": "Allow",
+#       "Principal": {
+#         "AWS": ["${data.aws_elb_service_account.main.arn}"]
+#       },
+#       "Action": "s3:PutObject",
+#       "Resource": "arn:aws:s3:::${var.app_name}-access-logs-${var.environment}/*"
+#     },
+#     {
+#       "Action": ["s3:PutObject"],
+#       "Effect": "Allow",
+#       "Resource": "arn:aws:s3:::${var.app_name}-access-logs-${var.environment}/*",
+# 	    "Condition": {
+#         "StringEquals": {
+#           "s3:x-amz-acl": "bucket-owner-full-control"
+#         }
+#       },
+#       "Principal": {
+# 	      "Service": "delivery.logs.amazonaws.com"
+#       }
+#     },
+#     {
+#       "Effect": "Allow",
+#       "Principal": {
+#         "Service": "delivery.logs.amazonaws.com"
+#       },
+#       "Action": "s3:GetBucketAcl",
+#       "Resource": "arn:aws:s3:::${var.app_name}-access-logs-${var.environment}"
+#     }
+#   ]
+# }
+# POLICY
+# }
 
-resource "aws_s3_bucket_ownership_controls" "ownership" {
+resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
   bucket = aws_s3_bucket.elb_logs.id
   rule {
     object_ownership = "BucketOwnerPreferred"
@@ -54,7 +54,7 @@ resource "aws_s3_bucket_ownership_controls" "ownership" {
 }
 
 resource "aws_s3_bucket_acl" "acl" {
-  depends_on = [aws_s3_bucket_ownership_controls.ownership]
+  depends_on = [aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership]
   bucket = aws_s3_bucket.elb_logs.id
   acl    = "private"
 }
