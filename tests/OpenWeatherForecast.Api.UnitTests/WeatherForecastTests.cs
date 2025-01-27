00f11243c0fe654fe86c3f54e2542b5431368286
Punam-Dev <77569830+Punam-Dev@users.noreply.github.com>
2025-01-27 22:25:26 +0530

    Update WeatherForecastTests.cs

--- tests/OpenWeatherForecast.Api.UnitTests/WeatherForecastTests.cs
+++ tests/OpenWeatherForecast.Api.UnitTests/WeatherForecastTests.cs
@@ -17,6 +17,6 @@ public class WeatherForecastTests : BaseIntegrationTest
         var response = await HttpClient.GetAsync("weatherforecast");
 
         // Assert
-        response.StatusCode.Should().Be(404);
+        response.StatusCode.Should().Be(HttpStatusCode.Created);
     }
 }
